38824f92114254e292bd732b380e710a3870cf39
Dinara-Pathiraja-ICD2O1-02 <Dinara.pathiraja@student.tdsb.on.ca>
2024-11-04 18:08:06 -0500

    Update PP1_5.py

--- PP1_5.py
+++ PP1_5.py
@@ -1,26 +1,46 @@
+'''
+    Lesson: Typecasting
+    Author: Mr. Kalisz
+    Date Creatd: Sept 23, 2024
+    Date Last Modified: Sept 23, 2024
+'''
 
 def q1():
   num1= input("Input an integer: ")
   num1=int(num1)
-  print(num1+3)
+  num2=(num1+3)
+  print(num2)
 
 def q2():
-  #Write Assignment code here
+  num1= input("Input an integer: ")
+  num2=(num1+4)
+  num3= float(num2)
+  num4= (num3+2)
+  print(num4)
 
 def q3():
-  #Write Assignment code here
+  radius= input("Input a radius: ")
+  radius= float(radius)
+  print(radius * 3.14)
 
 def q4():
-  #Write Assignment code here
+  num1= input("Input a number: ")
+  num1= float(num1)
+  num2= num1 * 12 
+  num2= int(num2)
+  print(num2)
 
 def q5():
-  #Write Assignment code here
+  num1= input("Input an integer: ")
+  num1= int(num1)
+  num2= (num1 + 5)
+  print (f"Your number + 5 is (num2)")
 
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
 
-q1()
-q2()
-q3()
-q4()
-q5()
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
