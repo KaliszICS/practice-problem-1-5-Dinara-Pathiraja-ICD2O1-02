c5898eebad7cfd85522c896b6b962213baffdb5d
Dinara-Pathiraja-ICD2O1-02 <Dinara.pathiraja@student.tdsb.on.ca>
2024-11-04 19:13:34 -0500

    Update PP1_5.py

--- PP1_5.py
+++ PP1_5.py
@@ -21,21 +21,21 @@ def q2():
 def q3():
   radius= input("Input a radius: ")
   radius1= float(radius)
-  print(3.14 * radius1^2 )
+  print(3.14 * {radius1}^2 )
 
 def q4():
 import math 
   num1= input("Input a number: ")
   num1= float(num1)
   num2= num1 * 12 
-  num2= math.floor(num2)
+  num2= math.floor{num2}
   print(num2)
 
 def q5():
   num1= input("Input an integer: ")
   num1= int(num1)
   var= (num1 + 5)
-  print (f"Your number + 5 is (var)")
+  print (f"Your number + 5 is {var}")
 
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
