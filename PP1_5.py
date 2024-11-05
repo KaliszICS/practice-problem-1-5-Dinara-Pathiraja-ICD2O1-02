47b36a6b1919f75381d519e51093fcbdcf798825
Dinara-Pathiraja-ICD2O1-02 <Dinara.pathiraja@student.tdsb.on.ca>
2024-11-04 22:21:21 -0500

    Update PP1_5.py

--- PP1_5.py
+++ PP1_5.py
@@ -5,19 +5,21 @@
     Date Last Modified: Nov 04, 2024
 '''
 
-def q1():
-  prompt= input("Input an integer: ")
-  num1=int(propmt)
-  num2=(num1+3)
-  print(num2)
+import math
 
+def q1():
+  prompt = input("Input an integer: ")
+  num1 = (int(prompt))
+  num2 = num1+3
+  print (num2)
+  
 def q2():
  prompt2 = input ("Input a number: ")
  num3 = ((prompt2) + '4')
  num4 = float(num3)
  num5 = num4+2
  print (num5)
-    
+
 def q3():
   prompt3 = input ("Input a radius: ")
   radius = (float (prompt3))
@@ -30,13 +32,13 @@ def q4():
   num7 = (num6*12)
   num8 = math.floor(num7)
   print (num8)
-
+  
+  
 def q5():
   prompt5 = input ("Input an integer: ")
   num9 = (int(prompt5))
   var1 = (num9 + 5)
   print (f"Your number + 5 is {var1}")
-
 
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
